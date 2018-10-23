88a41948e37ec9f1a2647d84012a3c35e52427a6
Niamke Dozier <s1niamke@gmail.com>
2018-10-23 15:45:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/niamkedozier/magnetometerapp/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.niamkedozier.magnetometerapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private TextView value;
+    private SensorManager sensorManager;
+    public static DecimalFormat DECIMAL_FORMATTER;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        value = (TextView) findViewById(R.id.value);
+        //define decimal
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        symbols.setDecimalSeparator('.');
+        DECIMAL_FORMATTER = new DecimalFormat("#.00", symbols);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        String data = value.getText().toString();
+
+        try{
+            FileOutputStream fileOut = openFileOutput("Data.txt", MODE_APPEND);
+            OutputStreamWriter outputWriter = new OutputStreamWriter(fileOut);
+            outputWriter.write(data);
+            outputWriter.write("\n");
+            outputWriter.close();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        System.out.println(MainActivity.this.getFilesDir().getAbsoluteFile());
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
+            //Gets all axis
+            float magX = event.values[0];
+            float magY = event.values[1];
+            float magZ = event.values[2];
+            double magnitude = Math.sqrt((magX * magX) + (magY * magY) + (magZ * magZ));
+            value.setText((DECIMAL_FORMATTER.format(magnitude) + " \u00B5Tesla"));
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public void BtnSave(View view){
+        String data = value.getText().toString();
+
+        //Tries to create a text file
+        try{
+            FileOutputStream fileOut = openFileOutput("Data.txt", MODE_APPEND);
+            OutputStreamWriter outputWriter = new OutputStreamWriter(fileOut);
+            outputWriter.write(data);
+            outputWriter.write("\n");
+            outputWriter.close();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
